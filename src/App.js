ef80e200765d5e42bfffe3884929b7cc789e4364
Alexes012 <mr.shkut3011@yandex.ru>
2020-03-18 20:50:39 +0300

    add quality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css'
 import p from "./App.css"
-import Message from "./Message.jsx";
+import Message from "./message/Message.jsx";
+import Quality from "./quality/quality";
+import Lexablabla from "./3hometask/3hometask";
+
+newTaskTitleRef = React.createRef();
+
+onAddTasksClick = () => {
+    let newText = this.newTaskTitleRef.current.value;
+    this.newTaskTitleRef.current.value = ""; //очистили инпут (присвоили пустую строку)
+    let newTask = {
+        title: newText,
+        isDone: false,
+        priority: "low"
+    };
+
+    let newTasks = [...this.state.tasks, newTask]
+    this.setState({
+        tasks: newTasks
+    });
+}
+
 function App() {
-  return (
-          <Message/>
-  );
+    return (
+        <div>
+            <Message/>
+            <Quality/>
+            <Lexablabla newTaskTitleRef={this.newTaskTitleRef} onAddTasksClick={this.onAddTasksClick}/>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/quality/quality.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import s from './quality.module.css'
+
+const QualityItem = (props) => {
+    return <div className={s.items}>{props.qualitys}</div>
+};
+
+const Quality = (props) => {
+
+    let QualityData = [
+        {quality: 'Юморист'},
+        {quality: 'Программист'},
+        {quality: 'Пофигист'}
+    ];
+
+    let QualityElements = QualityData
+        .map((quality, i) =>
+            <span className={i === 1 ? s.yo : s.yoyo}>
+                <QualityItem qualitys={quality.quality}/>
+            </span>);
+
+    console.log(QualityElements)
+    return (
+        <div className={s.quality}>
+            {QualityElements}
+        </div>
+    )
+};
+
+export default Quality;
